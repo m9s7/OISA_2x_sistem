6d571cccfb7073bebb51ae373f4883bc05960ebe
m9s7 <matijasreckovic97@gmail.com>
2022-12-03 22:48:24 +0100

    added requests to server @MOZZART

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"OISA_2x_sistem/maxbet"
-	"OISA_2x_sistem/soccerbet"
 	"fmt"
+	"strings"
+	"time"
 )
 
 func main() {
-	fmt.Println(maxbet.GetSportsCurrentlyOffered())
+	//fmt.Println(maxbet.GetSportsCurrentlyOffered())
 
 	//maxbet.Scrape("Fudbal")
 	//maxbet.Scrape("Košarka")
@@ -15,10 +15,18 @@ func main() {
 	//maxbet.Scrape("Stoni Tenis")
 	//maxbet.Scrape("eSport")
 
-	fmt.Println(soccerbet.GetSportsCurrentlyOffered())
+	//fmt.Println(soccerbet.GetSportsCurrentlyOffered())
 
 	//soccerbet.Scrape("Fudbal")
 	//soccerbet.Scrape("Košarka")
-	soccerbet.Scrape("Tenis")
+	//soccerbet.Scrape("Tenis")
 
+	//url := "https://www.mozzartbet.com/getRegularGroups"
+
+	currentDate := fmt.Sprint(time.Now().Format("01-02-2006"))
+	payload := strings.NewReader("{\"date\":\"" + currentDate + "\",\"sportIds\":[],\"competitionIds\":[],\"sort\":\"bycompetition\",\"specials\":null,\"subgames\":[],\"size\":1000,\"mostPlayed\":false,\"type\":\"betting\",\"numberOfGames\":0,\"activeCompleteOffer\":false,\"lang\":\"sr\",\"offset\":0}")
+
+	//req, _ := http.NewRequest("POST", url, payload)
+
+	fmt.Println(payload)
 }
--- maxbet/requests_to_server/requests.go
+++ maxbet/requests_to_server/requests.go
@@ -112,7 +112,7 @@ func GetMatchData(matchId int) map[string]interface{} {
 		return nil
 	}
 
-	req.Header.Add("cookie", "SESSION=eefc750c-d31a-4f64-a746-f66acb11dbd4; utkn=4a5f9352453828af2c0f99e44aed5f57; c_stamp=dca0b108-2243-458b-bf06-0aefcb701edb; _gcl_au=1.1.936261194.1663309076; _hjSessionUser_1350202=eyJpZCI6IjBiZDc1ZTIzLWQ5NWYtNWUwMC1hMzlmLWFiNDBlOTk0MzViNyIsImNyZWF0ZWQiOjE2NjMzMDkwNzYxNjcsImV4aXN0aW5nIjp0cnVlfQ==; _gcl_aw=GCL.1663591068.CjwKCAjwpqCZBhAbEiwAa7pXeVSXQsZpTd3Oj7oLS9eL82c_mATnK2z2_-Y93vrUwnCkF-nP9DoZjxoCVBgQAvD_BwE; _gac_UA-61991021-7=1.1663591068.CjwKCAjwpqCZBhAbEiwAa7pXeVSXQsZpTd3Oj7oLS9eL82c_mATnK2z2_-Y93vrUwnCkF-nP9DoZjxoCVBgQAvD_BwE; _ga_5RBFPRP28H=GS1.1.1663591067.1.1.1663591078.0.0.0; _ga=GA1.2.649193112.1663309076; _gid=GA1.2.535184041.1665315269; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=sr; ASP.NET_SessionId=i5kdqzaesig1djtrxw5hgxvq; _clck=1u50hws|1|f5k|0; _hjSession_1350202=eyJpZCI6ImZjN2RiYzM2LWQyZjUtNDU2Zi04ZTg1LTI2YTIyN2JhNmUyMiIsImNyZWF0ZWQiOjE2NjUzMTUyNzIyMzQsImluU2FtcGxlIjpmYWxzZX0=; _gat_UA-61991021-1=1; __cf_bm=UxnJKaEFrerBWR7NadBxg6XGPd682te1xtbz3zWIcHc-1665316239-0-AVz0cZ6bMOtACfdq4BzxD54DVYD9E3tvZ6xxu93ivK4IlBuwhJjFC0cKm0jfvndPU9avvSuCV2EB+FgV8cpvwxO6CvtcShQCFRYxnnoi4HVtBYC5kJZSTnwlVeJ9ZaGOig==; _gat=1; _clsk=1wswi4j|1665316242102|1|0|m.clarity.ms/collect")
+	req.Header.Add("cookie", "org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=sr")
 	req.Header.Add("authority", "www.maxbet.rs")
 	req.Header.Add("accept", "application/json, text/plain, */*")
 	req.Header.Add("accept-language", "en-US,en;q=0.9,bs;q=0.8")
